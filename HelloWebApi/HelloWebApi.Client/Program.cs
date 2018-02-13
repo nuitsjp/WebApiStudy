9cfdb1f485a1346bdd7c02882e89ab4fd3e936d6
nuits.jp <nuits.jp@live.jp>
2018-02-13 17:51:04 +0900

    Add HelloWebApi

---
+++ HelloWebApi/HelloWebApi.Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Rest;
+
+namespace HelloWebApi.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using (var client = new HelloWebApiClient(new Uri("http://localhost:9000"), new AnonymousCredentials()))
+            //{
+            //    foreach (var employee in client.GetEmployees())
+            //    {
+            //        Console.WriteLine($"Id:{employee.Id} Name:{employee.Name} BirthDay:{employee.BirthDay}");
+            //    }
+            //    Console.ReadLine();
+            //}
+        }
+
+        private class AnonymousCredentials : ServiceClientCredentials
+        {
+        }
+    }
+}
---
+++ HelloWebApi/HelloWebApi/Controllers/EmployeesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Swashbuckle.Swagger.Annotations;
+
+namespace HelloWebApi.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        // GET api/<controller>
+        [SwaggerOperation("GetEmployees")]
+        public IEnumerable<Employee> Get()
+        {
+            return new []
+            {
+                new Employee{Id = 1, Name = "山田 太郎", BirthDay = DateTime.Parse("1970/01/01")},
+                new Employee{Id = 2, Name = "佐藤 花子", BirthDay = DateTime.Parse("2000/10/11")}
+            };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
