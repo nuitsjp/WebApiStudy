d41c8f91c7f015ad2f3fa60e18c01bfdee7301a5
Atsushi Nakamura <nuits.jp@live.jp>
2018-02-14 23:54:00 +0900

    Add HelloWebApi.Client.

---
+++ HelloWebApi/HelloWebApi.Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelloWebApi.Service.Api;
+
+namespace HelloWebApi.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeeApi = new EmployeesApi("http://localhost:9000");
+            foreach (var employee in employeeApi.EmployeesGet())
+            {
+                Console.WriteLine($"Id:{employee.Id} Name:{employee.Name} BirthDay:{employee.BirthDay}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
